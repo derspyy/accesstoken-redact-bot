9b0eea053f35a2289e94221b5e3123513bbd06f9
derspyy <65043391+derspyy@users.noreply.github.com>
2023-05-16 15:37:47 -0300

    rewrite redact()

--- src/file.rs
+++ src/file.rs
@@ -5,17 +5,14 @@ use std::io::prelude::*;
 
 pub fn redact(url: &str, filename: &PathBuf) -> Result<(), Box<dyn Error>> {
     let content = ureq::get(url).call()?.into_string()?;
-    let words = content.split_inclusive(' ');
-    let mut word_vec: Vec<&str>  = content.split_inclusive(' ').collect();
-    for word in words.enumerate() {
-        if word.1 == "--accessToken " {
-            let token = word.0 + 1;
-            word_vec[token] = "*** ";
-        }
-    }
     let mut new_string = String::new();
-    for word in word_vec {
-        new_string.push_str(word);
+    let words: Vec<(usize, &str)> = content.split_inclusive(' ').enumerate().collect();
+    for word in words.iter().skip(1) {
+        let previous_word = word.0 - 1;
+        match words[previous_word].1  {
+            "--accessToken " => new_string.push_str("*** "),
+            _ => new_string.push_str(word.1),
+        }
     }
     let mut file = File::create(filename)?;
     file.write_all(new_string.as_bytes())?;
